b8ae9dc2b87ea3b0714193b98202ff3ba033f2c9
lowlighter <22963968+lowlighter@users.noreply.github.com>
2020-10-18 01:07:38 +0200

    Update test and build

--- tests/metrics.mjs
+++ tests/metrics.mjs
@@ -2,15 +2,20 @@
   import path from "path"
   import fs from "fs"
   import metrics from "../src/metrics.mjs"
+  import build from "../utils/build.mjs"
   import octokit from "@octokit/graphql"
   import OctokitRest from "@octokit/rest"
   import libxmljs from "libxmljs"
+  import url from "url"
+
+//Dirname
+  const __dirname =  path.join(path.dirname(url.fileURLToPath(import.meta.url)), "..", "action")
 
 //Die on unhandled rejections
   process.on("unhandledRejection", error => { throw error })
 
 //Load GitHub handlers
-  const token = process.argv.slice(2)[0] ?? ""
+  const token = process.argv.slice(2)[0] ?? "73a71c11ec07d9b114f5e2af26a3cdde1c6fe65a"
   const graphql = octokit.graphql.defaults({headers:{authorization: `token ${token}`}})
   const rest = new OctokitRest.Octokit({auth:token})
 
@@ -24,5 +29,11 @@
   const parsed = libxmljs.parseXml(rendered)
   if (parsed.errors.length)
     throw new Error(`Malformed SVG : \n${parsed.errors.join("\n")}`)
+
+//Ensure that action has been rebuild
+  const action = `${await fs.promises.readFile(`${__dirname}/dist/index.js`)}`
+  const code = await build()
+  if (action !== code)
+    throw new Error(`GitHub Action has not been rebuild. Run "npm run build" to solve this issue`)
 
   console.log("Test success !")
