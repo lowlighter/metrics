cd83e8514636464be7723fdf24efe4889383a666
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-02-06 13:11:23 +0100

    Fix mocked data

--- source/app/mocks/api/axios/get/nightscout.mjs
+++ source/app/mocks/api/axios/get/nightscout.mjs
@@ -8,24 +8,22 @@ export default function({faker, url}) {
         const directionArray = ["SingleUp", "DoubleUp", "FortyFiveUp", "Flat", "FortyFiveDown", "SingleDown", "DoubleDown"]
         return ({
             status:200,
-            data:{
-                data:[{
-                    _id:faker.git.commitSha().substring(0, 23),
-                    device:"xDrip-DexcomG5",
-                    date:lastInterval,
-                    dateString:new Date(lastInterval).toISOString(),
-                    sgv:faker.random.number({min:40, max:400}),
-                    delta:faker.random.number({min:-10, max:10}),
-                    direction:directionArray[Math.floor(Math.random() * directionArray.length)],
-                    type:"sgv",
-                    filtered:0,
-                    unfiltered:0,
-                    rssi:100,
-                    noise:1,
-                    sysTime:new Date(lastInterval).toISOString(),
-                    utcOffset:faker.random.number({min:-12, max:14})*60,
-                }],
-            },
+            data:[{
+                _id:faker.git.commitSha().substring(0, 23),
+                device:"xDrip-DexcomG5",
+                date:lastInterval,
+                dateString:new Date(lastInterval).toISOString(),
+                sgv:faker.random.number({min:40, max:400}),
+                delta:faker.random.number({min:-10, max:10}),
+                direction:directionArray[Math.floor(Math.random() * directionArray.length)],
+                type:"sgv",
+                filtered:0,
+                unfiltered:0,
+                rssi:100,
+                noise:1,
+                sysTime:new Date(lastInterval).toISOString(),
+                utcOffset:faker.random.number({min:-12, max:14})*60,
+            }],
         })
       }
   }
