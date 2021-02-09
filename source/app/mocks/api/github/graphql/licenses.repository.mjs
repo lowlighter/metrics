f7d28c329476f52446b7012d776b84ed702bcb53
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-02-09 19:05:47 +0100

    Add licenses plugin

---
+++ source/app/mocks/api/github/graphql/licenses.repository.mjs
@@ -0,0 +1,13 @@
+/**Mocked data */
+  export default function({faker, query, login = faker.internet.userName()}) {
+    console.debug("metrics/compute/mocks > mocking graphql api result > licenses/repository")
+    return ({
+      user:{
+        repository:{
+          licenseInfo:{ name:"MIT License"},
+          url:"https://github.com/lowlighter/metrics",
+          databaseId:293860197
+        }
+      }
+    })
+  }
---
+++ source/plugins/licenses/README.md
@@ -0,0 +1,19 @@
+### 📜 Licenses
+
+<table>
+  <td align="center">
+    <img src="">
+    <img width="900" height="1" alt="">
+  </td>
+</table>
+
+#### ℹ️ Examples workflows
+
+[➡️ Available options for this plugin](metadata.yml)
+
+```yaml
+- uses: lowlighter/metrics@latest
+  with:
+    # ... other options
+    plugin_licenses: yes
+```
