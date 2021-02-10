0a5abd34875e33b0313a375ca7548cdc55392b0d
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-02-10 21:09:11 +0100

    Continue plugin (2)

--- source/plugins/licenses/index.mjs
+++ source/plugins/licenses/index.mjs
@@ -11,21 +11,21 @@
 
         //Initialization
           const {user:{repository}} = await graphql(queries.licenses.repository({owner:data.repo.owner.login, name:data.repo.name, account}))
-          const result = {ratio, legal, default:repository.licenseInfo, licensed:{available:false}, text:{}, list:[], dependencies:0, known:0, unknown:0}
+          const result = {ratio, legal, default:repository.licenseInfo, licensed:{available:false}, text:{}, list:[], used:{}, dependencies:0, known:0, unknown:0}
+          const {used, text} = result
 
         //Register existing licenses properties
           const licenses = Object.fromEntries((await graphql(queries.licenses())).licenses.map(license => [license.key, license]))
           for (const license of Object.values(licenses))
-            [...license.limitations, ...license.conditions, ...license.permissions].flat().map(({key, label}) => result.text[key] = label)
+            [...license.limitations, ...license.conditions, ...license.permissions].flat().map(({key, label}) => text[key] = label)
 
         //Check if licensed exists
-          if (await imports.which("licensed")) {
+          if (await imports.which("github-licensed")) {
             //Setup for licensed
               console.debug(`metrics/compute/${login}/plugins > licenses > searching dependencies licenses using licensed`)
               const path = imports.paths.join(imports.os.tmpdir(), `${repository.databaseId}`)
               //Create temporary directory
                 console.debug(`metrics/compute/${login}/plugins > licenses > creating temp dir ${path}`)
-                await imports.fs.rmdir(path, {recursive:true})
                 await imports.fs.mkdir(path, {recursive:true})
               //Clone repository
                 console.debug(`metrics/compute/${login}/plugins > licenses > cloning temp git repository ${repository.url} to ${path}`)
@@ -42,30 +42,44 @@
                 ].join("\n"))
             //Spawn licensed process
               console.debug(`metrics/compute/${login}/plugins > licenses > running licensed`)
-              //;(await imports.run(`${prefix} `, {cwd:path}))
+              //;(await imports.run("github-licensed", {cwd:path}))
+            //Cleaning
+              console.debug(`metrics/compute/${login}/plugins > licensed > cleaning temp dir ${path}`)
+              await imports.fs.rmdir(path, {recursive:true})
           }
           else
             console.debug(`metrics/compute/${login}/plugins > licenses > licensed not available`)
 
         //List licenses properties
+          console.debug(`metrics/compute/${login}/plugins > licenses > compute licenses properties`)
           const base = {permissions:new Set(), limitations:new Set(), conditions:new Set()}
           const combined = {permissions:new Set(), limitations:new Set(), conditions:new Set()}
           for (const properties of Object.keys(base)) {
             //Base license
-              licenses[repository.licenseInfo.key][properties].map(({key}) => base[properties].add(key))
+              if (repository.licenseInfo)
+                licenses[repository.licenseInfo.key]?.[properties]?.map(({key}) => base[properties].add(key))
             //Combined licenses
               for (const {key} of result.list)
-                licenses[key][properties].map(({key}) => combined[properties].add(key))
+                licenses[key]?.[properties]?.map(({key}) => combined[properties].add(key))
           }
-
-        //Todo
-          //{name:"", key:"", value:NaN, count:NaN, x:NaN, color:""},
 
         //Merge limitations and conditions
           for (const properties of ["limitations", "conditions"])
-            result[properties] = [[...base[properties]].map(key => ({key, text:result.text[key], inherited:false})), [...combined[properties]].filter(key => !base[properties].has(key)).map(key => ({key, text:result.text[key], inherited:true}))].flat()
+            result[properties] = [[...base[properties]].map(key => ({key, text:text[key], inherited:false})), [...combined[properties]].filter(key => !base[properties].has(key)).map(key => ({key, text:text[key], inherited:true}))].flat()
         //Remove base permissions conflicting with inherited limitations
-          result.permissions = [...base.permissions].filter(key => !combined.limitations.has(key)).map(key => ({key, text:result.text[key]}))
+          result.permissions = [...base.permissions].filter(key => !combined.limitations.has(key)).map(key => ({key, text:text[key]}))
+
+        //Count used licenses
+          console.debug(`metrics/compute/${login}/plugins > licenses > computing ratio`)
+          for (const license of [...(repository.licenseInfo ? [repository.licenseInfo] : []), ...result.list])
+            used[license.key] = (used[license.key] ?? 0) + 1
+          const total = Object.values(used).reduce((a, b) => a + b, 0)
+        //Format used licenses and compute positions
+          const list = Object.entries(used).sort(([_an, a], [_bn, b]) => b - a).map(([key, count]) => ({name:licenses[key]?.nickname ?? licenses[key]?.name ?? licenses[key]?.spdxId ?? null, key, count, value:count/total, x:0, color:"#000000"}))
+          for (let i = 0; i < list.length; i++)
+            list.x = (list[i-1]?.x ?? 0) + (list[i-1]?.value ?? 0)
+        //Save ratios
+          result.list = list
 
         //Results
           return result
