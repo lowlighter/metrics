166f9090d2d13fa0f72463ec90202a7b553ecbc9
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2021-03-08 14:04:17 +0100

    Update index.mjs

--- source/app/action/index.mjs
+++ source/app/action/index.mjs
@@ -142,7 +142,8 @@
               catch (error) {
                 console.debug(error)
                 if (/not found/i.test(`${error}`)) {
-                  const {object:{sha}} = committer.rest.git.getRef({...github.context.repo, ref:`refs/heads/${committer.base}`})
+                  console.log(await committer.rest.git.getRef({...github.context.repo, ref:`refs/heads/${committer.base}`}))
+                  const {object:{sha}} = await committer.rest.git.getRef({...github.context.repo, ref:`refs/heads/${committer.base}`})
                   info("Committer base branch sha", sha)
                   await committer.rest.git.createRef({...github.context.repo, ref:`refs/heads/${committer.branch}`, sha})
                   info("Committer branch status", "(created)")
