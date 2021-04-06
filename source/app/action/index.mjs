a4fe50b2c0f14fc400f53b647f2f19293d0e32bc
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-04-06 23:17:50 +0200

    Accept "markdown" output type

--- source/app/action/index.mjs
+++ source/app/action/index.mjs
@@ -200,7 +200,7 @@
           info.break()
           info.group({metadata, name:"core", inputs:config})
           info("Plugin errors", die ? "(exit with error)" : "(displayed in generated image)")
-          const convert = ["jpeg", "png", "json"].includes(config["config.output"]) ? config["config.output"] : null
+          const convert = ["jpeg", "png", "json", "markdown"].includes(config["config.output"]) ? config["config.output"] : null
           Object.assign(q, config)
 
         //Base content
