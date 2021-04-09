540d4d372013cc4a75b3454a2f38bfb7ca860dde
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-04-09 20:09:35 +0200

    Add own organizations

--- source/plugins/notable/index.mjs
+++ source/plugins/notable/index.mjs
@@ -1,24 +1,42 @@
 //Setup
-  export default async function({login, q, imports, graphql, queries}, {enabled = false} = {}) {
+  export default async function({login, q, imports, graphql, data, account, queries}, {enabled = false} = {}) {
     //Plugin execution
       try {
         //Check if plugin is enabled and requirements are met
           if ((!enabled)||(!q.notable))
             return null
 
-        //Iterate through contributed repositories
-          let cursor = null
-          let pushed = 0
+        //Load inputs
+          imports.metadata.plugins.notable.inputs({data, account, q})
+
+        //Initialization
           const organizations = new Map()
-          do {
-            console.debug(`metrics/compute/${login}/base > retrieving contributed repositories after ${cursor}`)
-            const {user:{repositoriesContributedTo:{edges}}} = await graphql(queries.notable.contributions({login, after:cursor ? `after: "${cursor}"` : "", repositories:100}))
-            cursor = edges?.[edges?.length-1]?.cursor
-            edges.map(({node}) => node.isInOrganization ? organizations.set(node.owner.login, node.owner.avatarUrl) : null)
-            pushed = edges.length
-          } while ((pushed)&&(cursor))
-          const contributions = await Promise.all([...organizations.entries()].map(async([name, avatarUrl]) => ({name, avatar:await imports.imgb64(avatarUrl)})))
-          console.debug(`metrics/compute/${login}/base > found contributions to ${organizations.length} organizations`)
+
+        //Load organization memberships
+          try {
+            const {user:{organizations:{nodes}}} = await graphql(queries.notable.organizations({login}))
+            nodes.map(({login, avatarUrl}) => organizations.set(login, avatarUrl))
+          }
+          catch (error) {
+            console.debug(`metrics/compute/${login}/plugins > notable > failed to load organizations memberships: ${error}`)
+          }
+
+        //Iterate through contributed repositories from organizations
+          {
+            let cursor = null
+            let pushed = 0
+            do {
+              console.debug(`metrics/compute/${login}/plugins > notable > retrieving contributed repositories after ${cursor}`)
+              const {user:{repositoriesContributedTo:{edges}}} = await graphql(queries.notable.contributions({login, after:cursor ? `after: "${cursor}"` : "", repositories:100}))
+              cursor = edges?.[edges?.length-1]?.cursor
+              edges.map(({node}) => node.isInOrganization ? organizations.set(node.owner.login, node.owner.avatarUrl) : null)
+              pushed = edges.length
+            } while ((pushed)&&(cursor))
+          }
+
+        //Set contributions
+          const contributions = (await Promise.all([...organizations.entries()].map(async([name, avatarUrl]) => ({name, avatar:await imports.imgb64(avatarUrl)})))).sort((a, b) => a.name.localeCompare(b.name))
+          console.debug(`metrics/compute/${login}/plugins > notable > found contributions to ${organizations.length} organizations`)
 
         //Results
           return {contributions}
