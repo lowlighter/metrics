f4949b52f836bb5b309e7cc9f3e962e9938efef8
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2021-04-11 21:14:53 +0200

    Update index.mjs

--- source/app/action/index.mjs
+++ source/app/action/index.mjs
@@ -112,7 +112,7 @@
             }
             catch (error) {
               console.debug(error)
-              console.log("::warning::GitHub API did not send any \"x-oauth-scopes\" header back from provided \"token\". It means that your token may not be valid or you're using GITHUB_TOKEN which cannot be used since metrics will fetch data outside of this repository scope. Use a personal access token instead (see https://github.com/lowlighter/metrics/blob/master/.github/readme/partials/setup/action/setup.md for more informations).")
+              throw new Error("GitHub API did not send any \"x-oauth-scopes\" header back from provided \"token\". It means that your token may not be valid or you're using GITHUB_TOKEN which cannot be used since metrics will fetch data outside of this repository scope. Use a personal access token instead (see https://github.com/lowlighter/metrics/blob/master/.github/readme/partials/setup/action/setup.md for more informations).")
             }
           }
         //Extract octokits
