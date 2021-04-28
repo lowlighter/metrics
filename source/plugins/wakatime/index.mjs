eda35ca6f5b86b1d8acff80be0397e4b16a71895
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-04-28 21:04:07 +0000

    Add selfhosted wakatime integration (wakapi) (#276)

--- source/plugins/wakatime/index.mjs
+++ source/plugins/wakatime/index.mjs
@@ -1,44 +1,47 @@
 //Setup
-  export default async function({login, q, imports, data, account}, {enabled = false, token} = {}) {
-    //Plugin execution
-      try {
-        //Check if plugin is enabled and requirements are met
-          if ((!enabled)||(!q.wakatime))
-            return null
+export default async function ({ login, q, imports, data, account },{ enabled = false, token } = {}) {
+  //Plugin execution
+  try {
+    //Check if plugin is enabled and requirements are met
+    if (!enabled || !q.wakatime) return null;
 
-        //Load inputs
-          let {sections, days, limit} = imports.metadata.plugins.wakatime.inputs({data, account, q})
-          if (!limit)
-            limit = void(limit)
-          const range = {"7":"last_7_days", "30":"last_30_days", "180":"last_6_months", "365":"last_year"}[days] ?? "last_7_days"
+    //Load inputs
+    let { sections, days, limit, url, user } = imports.metadata.plugins.wakatime.inputs({ data, account, q });
+    if (!limit) limit = void limit;
+    const range =
+      {
+        7: "last_7_days",
+        30: "last_30_days",
+        180: "last_6_months",
+        365: "last_year",
+      }[days] ?? "last_7_days";
 
-        //Querying api and format result (https://wakatime.com/developers#stats)
-          console.debug(`metrics/compute/${login}/plugins > wakatime > querying api`)
-          const {data:{data:stats}} = await imports.axios.get(`https://wakatime.com/api/v1/users/current/stats/${range}?api_key=${token}`)
-          const result = {
-            sections,
-            days,
-            time:{
-              total:stats.total_seconds/(60*60),
-              daily:stats.daily_average/(60*60),
-            },
-            projects:stats.projects.map(({name, percent, total_seconds:total}) => ({name, percent:percent/100, total})).sort((a, b) => b.percent - a.percent).slice(0, limit),
-            languages:stats.languages.map(({name, percent, total_seconds:total}) => ({name, percent:percent/100, total})).sort((a, b) => b.percent - a.percent).slice(0, limit),
-            os:stats.operating_systems.map(({name, percent, total_seconds:total}) => ({name, percent:percent/100, total})).sort((a, b) => b.percent - a.percent).slice(0, limit),
-            editors:stats.editors.map(({name, percent, total_seconds:total}) => ({name, percent:percent/100, total})).sort((a, b) => b.percent - a.percent).slice(0, limit),
-          }
+    //Querying api and format result (https://wakatime.com/developers#stats)
+    console.debug(`metrics/compute/${login}/plugins > wakatime > querying api`);
+    const {data: { data: stats }} = await imports.axios.get(`${url}/api/v1/users/${user}/stats/${range}?api_key=${token}`);
+    const result = {
+      sections,
+      days,
+      time: {
+        total: stats.total_seconds / (60 * 60),
+        daily: stats.daily_average / (60 * 60),
+      },
+      projects:stats.projects.map(({name, percent, total_seconds:total}) => ({name, percent:percent/100, total})).sort((a, b) => b.percent - a.percent).slice(0, limit),
+      languages:stats.languages.map(({name, percent, total_seconds:total}) => ({name, percent:percent/100, total})).sort((a, b) => b.percent - a.percent).slice(0, limit),
+      os:stats.operating_systems.map(({name, percent, total_seconds:total}) => ({name, percent:percent/100, total})).sort((a, b) => b.percent - a.percent).slice(0, limit),
+      editors:stats.editors.map(({name, percent, total_seconds:total}) => ({name, percent:percent/100, total})).sort((a, b) => b.percent - a.percent).slice(0, limit),
+    };
 
-        //Result
-          return result
-      }
+    //Result
+    return result;
+  } catch (error) {
     //Handle errors
-      catch (error) {
-        let message = "An error occured"
-        if (error.isAxiosError) {
-          const status = error.response?.status
-          message = `API returned ${status}`
-          error = error.response?.data ?? null
-        }
-        throw {error:{message, instance:error}}
-      }
+    let message = "An error occured";
+    if (error.isAxiosError) {
+      const status = error.response?.status;
+      message = `API returned ${status}`;
+      error = error.response?.data ?? null;
+    }
+    throw {error:{message, instance:error }};
   }
+}
