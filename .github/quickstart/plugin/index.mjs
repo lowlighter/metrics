91ac38c7a496a895ddc7606919055f92d13b7ca3
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-07-30 20:49:31 +0200

    Update quickstart plugin skeleton

--- .github/quickstart/plugin/index.mjs
+++ .github/quickstart/plugin/index.mjs
@@ -1,15 +1,15 @@
 //Setup
-  export default async function({login, q, imports, data, computed, rest, graphql, queries, account}, {enabled = false} = {}) {
-    //Plugin execution
-      try {
-        //Check if plugin is enabled and requirements are met
-          if ((!enabled)||(!q.<%= name %>))
-            return null
-        //Results
-          return {}
-      }
-    //Handle errors
-      catch (error) {
-        throw {error:{message:"An error occured", instance:error}}
-      }
+export default async function({login, q, imports, data, computed, rest, graphql, queries, account}, {enabled = false} = {}) {
+  //Plugin execution
+  try {
+    //Check if plugin is enabled and requirements are met
+    if ((!enabled)||(!q.<%= name %>))
+      return null
+    //Results
+    return {}
   }
+  //Handle errors
+  catch (error) {
+    throw {error:{message:"An error occured", instance:error}}
+  }
+}
