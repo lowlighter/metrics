6fd0faa12630a54cf0cf8216bfc8bd3a0992d1f1
Nixinova <nixinova@gmail.com>
2021-08-15 09:32:23 +1200

    Re-add `.._recent_categories` option

--- source/plugins/languages/index.mjs
+++ source/plugins/languages/index.mjs
@@ -17,7 +17,7 @@ export default async function({login, data, imports, q, rest, account}, {enabled
     }
 
     //Load inputs
-    let {ignored, skipped, colors, aliases, details, threshold, limit, indepth, sections, categories, "recent.load":_recent_load, "recent.days":_recent_days} = imports.metadata.plugins.languages.inputs({data, account, q})
+    let {ignored, skipped, colors, aliases, details, threshold, limit, indepth, sections, categories, "recent.categories":_recent_categories, "recent.load":_recent_load, "recent.days":_recent_days} = imports.metadata.plugins.languages.inputs({data, account, q})
     threshold = (Number(threshold.replace(/%$/, "")) || 0) / 100
     skipped.push(...data.shared["repositories.skipped"])
     if (!limit)
@@ -57,7 +57,7 @@ export default async function({login, data, imports, q, rest, account}, {enabled
       //Recently used languages
       if ((sections.includes("recently-used"))&&(context.mode === "user")) {
         console.debug(`metrics/compute/${login}/plugins > languages > using recent analyzer`)
-        languages["stats.recent"] = await recent_analyzer({login, data, imports, rest, account}, {skipped, categories, days:_recent_days, load:_recent_load})
+        languages["stats.recent"] = await recent_analyzer({login, data, imports, rest, account}, {skipped, categories:_recent_categories, days:_recent_days, load:_recent_load})
       }
 
       //Indepth mode
