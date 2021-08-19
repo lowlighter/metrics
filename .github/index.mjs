fc9d97fd146c688fdccff897802207722ec51862
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-08-19 22:39:50 +0200

    Format .github code

--- .github/index.mjs
+++ .github/index.mjs
@@ -1,56 +1,56 @@
 //Imports
-  import ejs from "ejs"
-  import fs from "fs/promises"
-  import paths from "path"
-  import url from "url"
-  import sgit from "simple-git"
-  import metadata from "../source/app/metrics/metadata.mjs"
+import ejs from "ejs"
+import fs from "fs/promises"
+import paths from "path"
+import url from "url"
+import sgit from "simple-git"
+import metadata from "../source/app/metrics/metadata.mjs"
 
 //Mode
-  const [mode = "dryrun"] = process.argv.slice(2)
-  console.log(`Mode: ${mode}`)
+const [mode = "dryrun"] = process.argv.slice(2)
+console.log(`Mode: ${mode}`)
 
 //Paths
-  const __metrics = paths.join(paths.dirname(url.fileURLToPath(import.meta.url)), "..")
-  const __action = paths.join(__metrics, "source/app/action")
-  const __web = paths.join(__metrics, "source/app/web")
-  const __readme = paths.join(__metrics, ".github/readme")
+const __metrics = paths.join(paths.dirname(url.fileURLToPath(import.meta.url)), "..")
+const __action = paths.join(__metrics, "source/app/action")
+const __web = paths.join(__metrics, "source/app/web")
+const __readme = paths.join(__metrics, ".github/readme")
 
 //Git setup
-  const git = sgit(__metrics)
-  const staged = new Set()
+const git = sgit(__metrics)
+const staged = new Set()
 
 //Load plugins metadata
-  const {plugins, templates, packaged, descriptor} = await metadata({log:false})
+const {plugins, templates, packaged, descriptor} = await metadata({log:false})
 
 //Update generated files
-  async function update({source, output, options = {}}) {
-    //Regenerate file
-      console.log(`Generating ${output}`)
-      const content = await ejs.renderFile(source, {plugins, templates, packaged, descriptor}, {async:true, ...options})
-    //Save result
-      const file = paths.join(__metrics, output)
-      await fs.writeFile(file, content)
-    //Add to git
-      staged.add(file)
-  }
+async function update({source, output, options = {}}) {
+  //Regenerate file
+  console.log(`Generating ${output}`)
+  const content = await ejs.renderFile(source, {plugins, templates, packaged, descriptor}, {async:true, ...options})
+  //Save result
+  const file = paths.join(__metrics, output)
+  await fs.writeFile(file, content)
+  //Add to git
+  staged.add(file)
+}
 
 //Rendering
-  await update({source:paths.join(__readme, "README.md"), output:"README.md", options:{root:__readme}})
-  await update({source:paths.join(__readme, "partials/documentation/plugins.md"), output:"source/plugins/README.md"})
-  await update({source:paths.join(__readme, "partials/documentation/templates.md"), output:"source/templates/README.md"})
-  await update({source:paths.join(__action, "action.yml"), output:"action.yml"})
-  await update({source:paths.join(__web, "settings.example.json"), output:"settings.example.json"})
+await update({source:paths.join(__readme, "README.md"), output:"README.md", options:{root:__readme}})
+await update({source:paths.join(__readme, "partials/documentation/plugins.md"), output:"source/plugins/README.md"})
+await update({source:paths.join(__readme, "partials/documentation/templates.md"), output:"source/templates/README.md"})
+await update({source:paths.join(__action, "action.yml"), output:"action.yml"})
+await update({source:paths.join(__web, "settings.example.json"), output:"settings.example.json"})
 
 //Commit and push
-  if (mode === "publish") {
-    console.log(`Pushing staged changes: \n${[...staged].map(file => `  - ${file}`).join("\n")}`)
-    const gitted = await git
-      .addConfig("user.name", "github-actions[bot]")
-      .addConfig("user.email", "41898282+github-actions[bot]@users.noreply.github.com")
-      .add([...staged])
-      .commit("Auto-regenerate files")
-      .push("origin", "master")
-    console.log(gitted)
-  }
-  console.log("Success!")
+if (mode === "publish") {
+  console.log(`Pushing staged changes: \n${[...staged].map(file => `  - ${file}`).join("\n")}`)
+  const gitted = await git
+    .addConfig("user.name", "github-actions[bot]")
+    .addConfig("user.email", "41898282+github-actions[bot]@users.noreply.github.com")
+    .add([...staged])
+    .commit("Auto-regenerate files")
+    .push("origin", "master")
+  console.log(gitted)
+}
+console.log("Success!")
--- .github/release.mjs
+++ .github/release.mjs
@@ -1,56 +1,56 @@
 //Imports
-  import github from "@actions/github"
-  import paths from "path"
-  import url from "url"
-  import sgit from "simple-git"
+import github from "@actions/github"
+import paths from "path"
+import url from "url"
+import sgit from "simple-git"
 
 //Git setup
-  const __metrics = paths.join(paths.dirname(url.fileURLToPath(import.meta.url)), "..")
-  const git = sgit(__metrics)
+const __metrics = paths.join(paths.dirname(url.fileURLToPath(import.meta.url)), "..")
+const git = sgit(__metrics)
 
 //Setup octokit
-  const token = process.env.GITHUB_TOKEN
-  const rest = github.getOctokit(token)
+const token = process.env.GITHUB_TOKEN
+const rest = github.getOctokit(token)
 
 //Environment
-  const maintainer = "lowlighter"
-  const repository = process.env.GITHUB_REPOSITORY.match(/^(?<owner>[\s\S]+)[/](?<name>[\s\S]+)$/)?.groups ?? null
-  const version = process.env.GITHUB_COMMIT_MESSAGE.match(/(?<version>v\d+[.]\d+)/)?.groups?.version ?? null
+const maintainer = "lowlighter"
+const repository = process.env.GITHUB_REPOSITORY.match(/^(?<owner>[\s\S]+)[/](?<name>[\s\S]+)$/)?.groups ?? null
+const version = process.env.GITHUB_COMMIT_MESSAGE.match(/(?<version>v\d+[.]\d+)/)?.groups?.version ?? null
 
 //Check arguments
-  if ((!repository)||(!repository.name)||(!repository.owner))
-    throw new Error(`Could not parse repository "${process.env.GITHUB_REPOSITORY}"`)
-  console.log(`Repository: ${repository.owner}/${repository.name}`)
-  if (!version)
-    throw new Error(`Could not parse version from "${process.env.GITHUB_COMMIT_MESSAGE}"`)
-  console.log(`Version: ${version}`)
+if ((!repository)||(!repository.name)||(!repository.owner))
+  throw new Error(`Could not parse repository "${process.env.GITHUB_REPOSITORY}"`)
+console.log(`Repository: ${repository.owner}/${repository.name}`)
+if (!version)
+  throw new Error(`Could not parse version from "${process.env.GITHUB_COMMIT_MESSAGE}"`)
+console.log(`Version: ${version}`)
 
 //Load related pr
-  const {data:{items:prs}} = await rest.search.issuesAndPullRequests({
-    q:`repo:${repository.owner}/${repository.name} is:pr is:merged author:${maintainer} assignee:${maintainer} Release ${version} in:title`
-  })
+const {data:{items:prs}} = await rest.search.issuesAndPullRequests({
+  q:`repo:${repository.owner}/${repository.name} is:pr is:merged author:${maintainer} assignee:${maintainer} Release ${version} in:title`
+})
 
 //Ensure that there is exactly one pr matching
-  if (prs.length < 1)
-    throw new Error(`No matching prs found`)
-  if (prs.length > 1)
-    throw new Error(`Multiple prs found (${prs.length} matching)`)
-  const [patchnote] = prs
-  console.log(`Using pr#${patchnote.number}: ${patchnote.title}`)
+if (prs.length < 1)
+  throw new Error(`No matching prs found`)
+if (prs.length > 1)
+  throw new Error(`Multiple prs found (${prs.length} matching)`)
+const [patchnote] = prs
+console.log(`Using pr#${patchnote.number}: ${patchnote.title}`)
 
 //Check whether release already exists
-  try {
-    const {data:{id}} = await rest.repos.getReleaseByTag({owner:repository.owner, repo:repository.name, tag:version})
-    console.log(`Release ${version} already exists (#${id}), will replace it`)
-    await rest.repos.deleteRelease({owner:repository.owner, repo:repository.name, release_id:id})
-    console.log(`Deleting tag ${version}`)
-    await git.push(["--delete", "origin", version])
-    await new Promise(solve => setTimeout(solve, 15*1000))
-  }
-  catch {
-    console.log(`Release ${version} does not exists yet, will create it`)
-  }
+try {
+  const {data:{id}} = await rest.repos.getReleaseByTag({owner:repository.owner, repo:repository.name, tag:version})
+  console.log(`Release ${version} already exists (#${id}), will replace it`)
+  await rest.repos.deleteRelease({owner:repository.owner, repo:repository.name, release_id:id})
+  console.log(`Deleting tag ${version}`)
+  await git.push(["--delete", "origin", version])
+  await new Promise(solve => setTimeout(solve, 15*1000))
+}
+catch {
+  console.log(`Release ${version} does not exists yet, will create it`)
+}
 
 //Publish release
-  await rest.repos.createRelease({owner:repository.owner, repo:repository.name, tag_name:version, name:`Version ${version.replace(/^v/g, "")}`, body:patchnote.body})
-  console.log(`Successfully published`)
+await rest.repos.createRelease({owner:repository.owner, repo:repository.name, tag_name:version, name:`Version ${version.replace(/^v/g, "")}`, body:patchnote.body})
+console.log(`Successfully published`)
