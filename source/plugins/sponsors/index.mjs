019262cb51145d669ad50d94cfab27aaa3dcc463
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-09-29 23:10:50 -0400

    fix(plugins/sponsors): handle null tiers

--- source/plugins/sponsors/index.mjs
+++ source/plugins/sponsors/index.mjs
@@ -13,7 +13,7 @@ export default async function({login, q, imports, data, graphql, queries, accoun
     const {[account]:{sponsorsListing:{fullDescription, activeGoal}, sponsorshipsAsMaintainer:{nodes, totalCount:count}}} = await graphql(queries.sponsors({login, account}))
     const about = await imports.markdown(fullDescription, {mode:"multiline"})
     const goal = activeGoal ? {progress:activeGoal.percentComplete, title:activeGoal.title, description:await imports.markdown(activeGoal.description)} : null
-    const list = nodes.map(({sponsorEntity:{login, avatarUrl}, tier:{monthlyPriceInDollars:amount}}) => ({login, avatarUrl, amount}))
+    const list = nodes.map(({sponsorEntity:{login, avatarUrl}, tier}) => ({login, avatarUrl, amount:tier?.monthlyPriceInDollars}))
     await Promise.all(list.map(async user => user.avatar = await imports.imgb64(user.avatarUrl)))
 
     //Results
