96ae8d1dfcc340be4e9a6d4d7194b8c87db086c9
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2021-09-29 20:17:38 -0400

    feat(plugins/sponsors): add new sponsors plugin (#548)

---
+++ source/plugins/sponsors/README.md
@@ -0,0 +1,25 @@
+### 💕 GitHub Sponsors
+
+The *sponsors* plugin lets you display your sponsors and introduction text from [GitHub sponsors](https://github.com/sponsors/).
+
+<table>
+  <td align="center">
+    <img src="https://github.com/lowlighter/lowlighter/blob/master/metrics.plugin.sponsors.svg">
+    <details><summary>With GitHub sponsors introduction</summary>
+      <img src="https://github.com/lowlighter/lowlighter/blob/master/metrics.plugin.sponsors.full.svg">
+    </details>
+    <img width="900" height="1" alt="">
+  </td>
+</table>
+
+#### ℹ️ Examples workflows
+
+[➡️ Available options for this plugin](metadata.yml)
+
+```yaml
+- uses: lowlighter/metrics@latest
+  with:
+    # ... other options
+    plugin_sponsors: yes
+    plugin_sponsors_sections: goal, about # Display goal and about sections
+```
---
+++ source/plugins/sponsors/index.mjs
@@ -0,0 +1,26 @@
+//Setup
+export default async function({login, q, imports, data, graphql, queries, account}, {enabled = false} = {}) {
+  //Plugin execution
+  try {
+    //Check if plugin is enabled and requirements are met
+    if ((!enabled)||(!q.sponsors))
+      return null
+
+    //Load inputs
+    const {sections} = await imports.metadata.plugins.sponsors.inputs({data, account, q})
+
+    //Query sponsors and goal
+    const {[account]:{sponsorsListing:{fullDescription, activeGoal}, sponsorshipsAsMaintainer:{nodes, totalCount:count}}} = await graphql(queries.sponsors({login, account}))
+    const about = await imports.markdown(fullDescription, {mode:"multiline"})
+    const goal = activeGoal ? {progress:activeGoal.percentComplete, title:activeGoal.title, description:await imports.markdown(activeGoal.description)} : null
+    const list = nodes.map(({sponsorEntity:{login, avatarUrl}, tier:{monthlyPriceInDollars:amount}}) => ({login, avatarUrl, amount}))
+    await Promise.all(list.map(async user => user.avatar = await imports.imgb64(user.avatarUrl)))
+
+    //Results
+    return {sections, about, list, count, goal}
+  }
+  //Handle errors
+  catch (error) {
+    throw {error:{message:"An error occured", instance:error}}
+  }
+}
