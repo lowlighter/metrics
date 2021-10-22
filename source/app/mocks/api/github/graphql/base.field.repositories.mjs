ead8bd2eb797c0b3cdd1ecd41e561ecf781b4d4a
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2021-10-22 00:05:00 -0400

    feat(plugins/base): split base queries to reduce GitHub API timeouts (#584)

---
+++ source/app/mocks/api/github/graphql/base.field.repositories.mjs
@@ -0,0 +1,9 @@
+/**Mocked data */
+export default function({faker, query, login = faker.internet.userName()}) {
+  console.debug("metrics/compute/mocks > mocking graphql api result > base/user")
+  return ({
+    user:{
+      repositories:{totalCount:faker.datatype.number(100), totalDiskUsage:faker.datatype.number(100000)},
+    },
+  })
+}
