5498cdc8c1fd27efeae4fd98e63ea1c1d9ba9c23
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2021-11-04 00:24:17 -0400

    feat(plugins/notable): add `indepth` mode (#635) [skip ci]

--- source/plugins/notable/index.mjs
+++ source/plugins/notable/index.mjs
@@ -1,5 +1,5 @@
 //Setup
-export default async function({login, q, imports, graphql, data, account, queries}, {enabled = false} = {}) {
+export default async function({login, q, imports, rest, graphql, data, account, queries}, {enabled = false, extras = false} = {}) {
   //Plugin execution
   try {
     //Check if plugin is enabled and requirements are met
@@ -7,10 +7,10 @@ export default async function({login, q, imports, graphql, data, account, querie
       return null
 
     //Load inputs
-    let {filter, repositories, from} = imports.metadata.plugins.notable.inputs({data, account, q})
+    let {filter, repositories, from, indepth} = imports.metadata.plugins.notable.inputs({data, account, q})
 
     //Iterate through contributed repositories
-    const notable = new Map()
+    const commits = []
     {
       let cursor = null
       let pushed = 0
@@ -21,14 +21,88 @@ export default async function({login, q, imports, graphql, data, account, querie
         edges
           .filter(({node}) => ({all:true, organization:node.isInOrganization, user:!node.isInOrganization}[from]))
           .filter(({node}) => imports.ghfilter(filter, {name:node.nameWithOwner, user:node.owner.login, stars:node.stargazers.totalCount, watchers:node.watchers.totalCount, forks:node.forks.totalCount}))
-          .map(({node}) => notable.set((repositories || !node.isInOrganization) ? node.nameWithOwner : node.owner.login, {organization:node.isInOrganization, avatarUrl:node.owner.avatarUrl}))
+          .map(({node}) => commits.push({handle:node.nameWithOwner, stars:node.stargazers.totalCount, organization:node.isInOrganization, avatarUrl:node.owner.avatarUrl}))
         pushed = edges.length
       } while ((pushed) && (cursor))
     }
 
     //Set contributions
-    const contributions = (await Promise.all([...notable.entries()].map(async ([name, {avatarUrl, organization}]) => ({name, avatar:await imports.imgb64(avatarUrl), organization})))).sort((a, b) => a.name.localeCompare(b.name))
+    let contributions = (await Promise.all(commits.map(async ({handle, stars, avatarUrl, organization}) => ({name:handle.split("/").shift(), handle, stars, avatar:await imports.imgb64(avatarUrl), organization})))).sort((a, b) => a.name.localeCompare(b.name))
     console.debug(`metrics/compute/${login}/plugins > notable > found ${contributions.length} notable contributions`)
+
+    //Extras features
+    if (extras) {
+      //Indepth
+      if (indepth) {
+        console.debug(`metrics/compute/${login}/plugins > notable > indepth`)
+        for (const contribution of contributions) {
+          //Prepare data
+          const {handle, stars} = contribution
+          const [owner, repo] = handle.split("/")
+          try {
+            //Count total commits on repository
+            const {repository:{defaultBranchRef:{target:{history}}}} = await graphql(queries.notable.commits({owner, repo}))
+            contribution.history = history.totalCount
+
+            //Load maintainers (errors probably means that token is not allowed to list contributors hence not a maintainer of said repo)
+            const {data:collaborators} = await rest.repos.listCollaborators({owner, repo}).catch(() => ({data:[]}))
+            const maintainers = collaborators.filter(({role_name:role}) => ["admin", "maintain", "write"].includes(role)).map(({login}) => login)
+
+            //Count total commits of user
+            const {data:contributions = []} = await rest.repos.getContributorsStats({owner, repo})
+            const commits = contributions.filter(({author}) => author.login.toLocaleLowerCase() === login.toLocaleLowerCase()).reduce((a, {total:b}) => a + b, 0)
+
+            //Save user data
+            contribution.user = {
+              commits,
+              percentage:commits/contribution.history,
+              maintainer:maintainers.includes(login),
+              get stars() {
+                return this.maintainer ? stars : this.percentage*stars
+              }
+            }
+            console.debug(`metrics/compute/${login}/plugins > notable > indepth > successfully processed ${owner}/${repo}`)
+          }
+          catch (error) {
+            console.debug(error)
+            console.debug(`metrics/compute/${login}/plugins > notable > indepth > failed to compute for ${owner}/${repo}`)
+          }
+        }
+      }
+    }
+
+    //Aggregate contributions
+    if (from !== "all") {
+      console.debug(`metrics/compute/${login}/plugins > notable > aggregating results`)
+      contributions = contributions.filter(({organization}) => (from === "organization")&&(organization))
+      const aggregated = new Map()
+      for (const {name, handle, avatar, organization, stars, ..._extras} of contributions) {
+        const key = repositories ? handle : name
+        if (aggregated.has(key)) {
+          const aggregate = aggregated.get(key)
+          aggregate.aggregated++
+          if (extras) {
+            const {history = 0, user:{commits = 0, percentage = 0, maintainer = false} = {}} = _extras
+            aggregate.history = aggregate.history ?? 0
+            aggregate.history += history
+            aggregate.user = aggregate.user ?? {}
+            aggregate.user.commits += commits
+            aggregate.user.percentage += percentage
+            aggregate.user.maintainer = aggregate.user.maintainer || maintainer
+          }
+        }
+        else
+          aggregated.set(key, {name:key, handle, avatar, organization, stars, aggregated:1, ..._extras})
+      }
+      contributions = [...aggregated.values()]
+      if (extras) {
+        //Normalize contribution percentage
+        contributions.map(aggregate => aggregate.user ? aggregate.user.percentage /= aggregate.aggregated : null)
+        //Sort contribution by maintainer first and then by contribution percentage
+        contributions = contributions.sort((a, b) => ((b.user?.percentage + b.user?.maintainer) || 0) - ((a.user?.percentage + a.user?.maintainer) || 0))
+      }
+    }
+
 
     //Results
     return {contributions}
