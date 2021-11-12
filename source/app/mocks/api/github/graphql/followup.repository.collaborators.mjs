0a9df6270b18988c562856c1950c624117dd6136
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2021-11-12 00:34:40 -0500

    feat(plugins/followup): add `indepth` mode (#627)

---
+++ source/app/mocks/api/github/graphql/followup.repository.collaborators.mjs
@@ -0,0 +1,11 @@
+/**Mocked data */
+export default function({faker, query, login = faker.internet.userName()}) {
+  console.debug("metrics/compute/mocks > mocking graphql api result > followup/repository/collaborators")
+  return ({
+    repository:{
+      collaborators:{
+        nodes:["github-user"]
+      }
+    },
+  })
+}
