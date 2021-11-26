ca104999d3fa71cae39d54bf5fa58a05c86f9d9c
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-11-25 19:20:50 -0500

    feat(plugins/traffic): add `plugin_traffic_skipped`

--- source/plugins/traffic/index.mjs
+++ source/plugins/traffic/index.mjs
@@ -7,7 +7,8 @@ export default async function({login, imports, data, rest, q, account}, {enabled
       return null
 
     //Load inputs
-    imports.metadata.plugins.traffic.inputs({data, account, q})
+    let {skipped} = imports.metadata.plugins.traffic.inputs({data, account, q})
+    skipped.push(...data.shared["repositories.skipped"])
 
     //Repositories
     const repositories = data.user.repositories.nodes.map(({name:repo, owner:{login:owner}}) => ({repo, owner})) ?? []
@@ -15,7 +16,7 @@ export default async function({login, imports, data, rest, q, account}, {enabled
     //Get views stats from repositories
     console.debug(`metrics/compute/${login}/plugins > traffic > querying api`)
     const views = {count:0, uniques:0}
-    const response = await Promise.all(repositories.map(({repo, owner}) => rest.repos.getViews({owner, repo})))
+    const response = [...await Promise.allSettled(repositories.map(({repo, owner}) => (skipped.includes(repo.toLocaleLowerCase())) || (skipped.includes(`${owner}/${repo}`.toLocaleLowerCase())) ? {} : rest.repos.getViews({owner, repo})))].filter(({status}) => status === "fulfilled").map(({value}) => value)
 
     //Compute views
     console.debug(`metrics/compute/${login}/plugins > traffic > computing stats`)
