64427b0a919db4ad46bef5726877cb2bc29322e3
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2022-01-14 05:41:13 +0100

    tests: auto-generated from `examples.yml` (#773) [skip ci]

--- .github/examples.mjs
+++ .github/examples.mjs
@@ -10,6 +10,7 @@ import yaml from "js-yaml"
 const __metrics = paths.join(paths.dirname(url.fileURLToPath(import.meta.url)), "..")
 const __templates = paths.join(paths.join(__metrics, "source/templates/"))
 const __plugins = paths.join(paths.join(__metrics, "source/plugins/"))
+const __test_plugins = paths.join(paths.join(__metrics, "tests/plugins"))
 
 //Load plugins metadata
 const {plugins, templates} = await metadata({log:false, diff:true})
@@ -18,25 +19,62 @@ async function plugin(id) {
   const path = paths.join(__plugins, id)
   const readme = paths.join(path, "README.md")
   const examples = paths.join(path, "examples.yml")
+  const tests = paths.join(__test_plugins, id)
   return {
     readme:{
       path:readme,
       content:`${await fs.readFile(readme)}`
+    },
+    tests:{
+      dir:tests,
+      file:paths.join(tests, "tests.yml")
     },
     examples:fss.existsSync(examples) ? yaml.load(await fs.readFile(examples), "utf8") ?? [] : [],
     options:plugins[id].readme.table
   }
 }
 
+const secrets = {
+  $regex:/\$\{\{\s*secrets\.(?<secret>\w+)\s*\}\}/,
+  METRICS_TOKEN:"MOCKED_TOKEN",
+  METRICS_BOT_TOKEN:"MOCKED_TOKEN",
+  PAGESPEED_TOKEN:"MOCKED_TOKEN",
+  SPOTIFY_TOKENS:"MOCKED_CLIENT_ID, MOCKED_CLIENT_SECRET, MOCKED_REFRESH_TOKEN",
+  YOUTUBE_MUSIC_TOKEN:"SAPISID=MOCKED_COOKIE; OTHER_PARAM=OTHER_VALUE;",
+  LASTFM_TOKEN:"MOCKED_TOKEN",
+  NIGHTSCOUT_URL:"https://testapp.herokuapp.com/",
+  WAKATIME_TOKEN:"MOCKED_TOKEN",
+  WAKATIME_TOKEN_NO_PROJECTS:"MOCKED_TOKEN_NO_PROJECTS",
+  TWITTER_TOKEN:"MOCKED_TOKEN",
+  RAPIDAPI_TOKEN:"MOCKED_TOKEN",
+}
+
 //Plugins
 for (const id of Object.keys(plugins)) {
-  const {examples, options, readme} = await plugin(id)
+  const {examples, options, readme, tests} = await plugin(id)
+
   //Plugin readme
   await fs.writeFile(readme.path, readme.content
     .replace(/(<!--examples-->)[\s\S]*(<!--\/examples-->)/g, `$1\n${examples.map(({test, prod, ...step}) => ["```yaml", yaml.dump(step), "```"].join("\n")).join("\n")}\n$2`)
     .replace(/(<!--options-->)[\s\S]*(<!--\/options-->)/g, `$1\n${options}\n$2`)
   )
   //Plugin tests
+  await fs.mkdir(tests.dir, { recursive: true });
+  await fs.writeFile(tests.file, yaml.dump(examples.map(({prod, test = {}, name = "", ...step}) => {
+    const result = {name:`${plugins[id].name} - ${name}`, ...step, ...test}
+    test.with ??= {}
+    for (const [k, v] of Object.entries(result.with)) {
+      if (k in test.with)
+        result.with[k] = test.with[k]
+      if (secrets.$regex.test(v))
+        result.with[k] = v.replace(secrets.$regex, secrets[v.match(secrets.$regex)?.groups?.secret])
+    }
+    if (!result.with.base)
+      delete result.with.base
+    delete result.with.filename
+    return result
+  })))
+
 }
 
 //Templates
