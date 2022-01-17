7c7f2356848df41448af2503ce3786a050020559
lowlighter <22963968+lowlighter@users.noreply.github.com>
2022-01-17 12:55:54 -0500

    ci: fix user

--- .github/scripts/build.mjs
+++ .github/scripts/build.mjs
@@ -172,8 +172,12 @@ function testcase(name, env, args) {
   if (env === "prod") {
     result.if = "${{ success() || failure() }}"
     result.uses = "lowlighter/metrics@master"
-    Object.assign(result.with, { plugins_errors_fatal: "yes", output_action: "none", delay: 120 })
+    Object.assign(result.with, { output_action: "none", delay: 120 })
 
+    for (const {property, value} of [{property: "user", value: "lowlighter"}, {property:"plugins_errors_fatal", value: "yes"}]) {
+      if (!(property in result.with))
+        result.with[property] = value
+    }
     if ((context.with?.output_action) && (context.with?.committer_branch === "examples"))
       Object.assign(result.with, { output_action: context.with.output_action, committer_branch: "examples" })
   }
