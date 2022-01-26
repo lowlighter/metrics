d764d510d3ade372d53db2ebd5a8841b2ead1b68
lowlighter <22963968+lowlighter@users.noreply.github.com>
2022-01-25 23:57:20 -0500

    fix(app/metrics): no default value for `config_timezone` and `use_prebuilt_image`

--- source/app/metrics/metadata.mjs
+++ source/app/metrics/metadata.mjs
@@ -228,7 +228,7 @@ metadata.plugin = async function({__plugins, __templates, name, logger}) {
           key,
           {
             comment:comments[key] ?? `# ${value.description}`,
-            descriptor:yaml.dump({[key]:Object.fromEntries(Object.entries(value).filter(([key]) => ["description", "default", "required"].includes(key)).map(([k, v]) => k === "description" ? [k, v.split("\n")[0]] : k === "default" ? [k, (/^\$\{\{[\s\S]+\}\}$/.test(v))||(k === "config_preset") ? v : "<default-value>"] : [k, v]))}, {quotingType:'"', noCompatMode:true}),
+            descriptor:yaml.dump({[key]:Object.fromEntries(Object.entries(value).filter(([key]) => ["description", "default", "required"].includes(key)).map(([k, v]) => k === "description" ? [k, v.split("\n")[0]] : k === "default" ? [k, (/^\$\{\{[\s\S]+\}\}$/.test(v))||(["config_preset", "config_timezone", "use_prebuilt_image"].includes(k)) ? v : "<default-value>"] : [k, v]))}, {quotingType:'"', noCompatMode:true}),
           },
         ]),
       )
