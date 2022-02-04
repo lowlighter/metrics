1508a1e88e0c6c8fbca019f2b435c909060ea50a
lowlighter <22963968+lowlighter@users.noreply.github.com>
2022-02-03 23:43:34 -0500

    fix: add mocks

---
+++ tests/mocks/api/github/graphql/notable.issues.mjs
@@ -0,0 +1,35 @@
+/**Mocked data */
+export default function({ faker, query, login = faker.internet.userName() }) {
+  console.debug("metrics/compute/mocks > mocking graphql api result > notable/issues")
+  return /after: "MOCKED_CURSOR"/m.test(query)
+    ? ({
+      user: {
+        issues: {
+          edges: [],
+        },
+        pullRequests: {
+          edges: [],
+        },
+      },
+    })
+    : ({
+      user: {
+        issues: {
+          totalCount: faker.datatype.number(1000),
+          edges: [
+            {
+              cursor: "MOCKED_CURSOR",
+              node: {
+                repository:{
+                  nameWithOwner: `${faker.internet.userName()}/${faker.lorem.slug()}`
+                }
+              },
+            },
+          ],
+        },
+        get pullRequests() {
+          return this.issues
+        }
+      },
+    })
+}
