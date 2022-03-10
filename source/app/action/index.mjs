42de3f07f64d01f36fd2f9cd7dfc73e10401d8cc
lowlighter <22963968+lowlighter@users.noreply.github.com>
2022-03-09 19:19:39 -0500

    fix(app/action): avoid dying when formats is not defined

--- source/app/action/index.mjs
+++ source/app/action/index.mjs
@@ -136,8 +136,8 @@ async function retry(func, {retries = 1, delay = 0} = {}) {
       ...config
     } = metadata.plugins.core.inputs.action({core, preset})
     const q = {...query, ...(_repo ? {repo:_repo} : null), template}
-    const _output = ["svg", "jpeg", "png", "json", "markdown", "markdown-pdf", "insights"].includes(config["config.output"]) ? config["config.output"] : metadata.templates[template].formats[0] ?? null
-    const filename = _filename.replace(/[*]/g, {jpeg:"jpg", markdown:"md", "markdown-pdf":"pdf", insights:"html"}[_output] ?? _output)
+    const _output = ["svg", "jpeg", "png", "json", "markdown", "markdown-pdf", "insights"].includes(config["config.output"]) ? config["config.output"] : metadata.templates[template]?.formats?.[0] ?? null
+    const filename = _filename.replace(/[*]/g, {jpeg:"jpg", markdown:"md", "markdown-pdf":"pdf", insights:"html"}[_output] ?? _output ?? "*")
 
     //Docker image
     if (_image)
