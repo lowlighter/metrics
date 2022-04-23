c0daf53393cf167c786874a7c622850678d86d43
lowlighter <22963968+lowlighter@users.noreply.github.com>
2022-04-23 11:56:31 -0400

    feat(plugins/sponsors): fetch more than 100 sponsors/past sponsors and display organizations

--- tests/mocks/api/github/graphql/sponsors.all.mjs
+++ tests/mocks/api/github/graphql/sponsors.all.mjs
@@ -1,9 +1,19 @@
 /**Mocked data */
 export default function({ faker, query, login = faker.internet.userName() }) {
   console.debug("metrics/compute/mocks > mocking graphql api result > sponsors/all")
-  return ({
+  return /after: "MOCKED_CURSOR"/m.test(query) ?
+    ({
+      user: {
+        sponsorsActivities: {
+          edges:[],
+          nodes:[],
+        }
+      }
+    })
+  : ({
     user: {
       sponsorsActivities: {
+        edges: new Array(10).fill("MOCKED_CURSOR"),
         nodes: new Array(10).fill(null).map(_ => ({
           sponsor: {
             login: faker.internet.userName(),
