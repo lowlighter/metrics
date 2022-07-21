60713f85d78097e7b9f16d4c37908fc0800b6faf
lowlighter <22963968+lowlighter@users.noreply.github.com>
2022-07-20 22:43:28 -0400

    fix stuff

--- source/plugins/skyline/index.mjs
+++ source/plugins/skyline/index.mjs
@@ -14,9 +14,11 @@ export default async function({login, q, imports, data, account}, {enabled = fal
     }
     const width = 454 * (1 + data.large)
     const height = 284
-    const {url, ready, wait, hide} = settings
 
-    //Start puppeteer and navigate to skyline.github.com
+    //Load settings (force default if extras is disabled)
+    const {url, ready, wait, hide} = imports.metadata.plugins.skyline.extras("settings", {extras, error: false}) ? settings : JSON.parse(imports.metadata.plugins.skyline.inputs.settings.default)
+
+    //Start puppeteer and navigate to skyline website
     console.debug(`metrics/compute/${login}/plugins > skyline > starting browser`)
     const browser = await imports.puppeteer.launch()
     console.debug(`metrics/compute/${login}/plugins > skyline > started ${await browser.version()}`)
@@ -24,13 +26,18 @@ export default async function({login, q, imports, data, account}, {enabled = fal
     await page.setViewport({width, height})
 
     //Load page
+    if (!url)
+      throw {error:{message:"Skyline URL is not set"}}
     console.debug(`metrics/compute/${login}/plugins > skyline > loading ${url.replaceAll("${login}", login).replaceAll("${year}", year)}`)
     await page.goto(url.replaceAll("${login}", login).replaceAll("${year}", year), {timeout: 90 * 1000})
     console.debug(`metrics/compute/${login}/plugins > skyline > waiting for initial render`)
     const frame = page.mainFrame()
-    await page.waitForFunction(ready.replaceAll("${login}", login).replaceAll("${year}", year), {timeout: 90 * 1000})
-    await new Promise(solve => setTimeout(solve, wait*1000))
-    await frame.evaluate(() => [...document.querySelectorAll(hide)].map(element => element.style.display = "none"))
+    if (ready)
+      await page.waitForFunction(ready.replaceAll("${login}", login).replaceAll("${year}", year), {timeout: 90 * 1000})
+    if ((wait)&&(wait > 0))
+      await new Promise(solve => setTimeout(solve, wait*1000))
+    if (hide)
+      await frame.evaluate(hide => [...document.querySelectorAll(hide)].map(element => element.style.display = "none"), hide)
 
     //Generate gif
     console.debug(`metrics/compute/${login}/plugins > skyline > generating frames`)
