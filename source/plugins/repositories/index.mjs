63c194924c1f7fbb8144db09f05fa2ef80f1fb96
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2022-08-07 17:05:21 +0200

    feat(plugins/repositories): add options for starred/random repositories (#1179)

--- source/plugins/repositories/index.mjs
+++ source/plugins/repositories/index.mjs
@@ -7,23 +7,66 @@ export default async function({login, q, imports, graphql, queries, data, accoun
       return null
 
     //Load inputs
-    let {featured, pinned} = imports.metadata.plugins.repositories.inputs({data, account, q})
+    let {featured, pinned, starred, random, order, affiliations:_affiliations} = imports.metadata.plugins.repositories.inputs({data, account, q})
+    const affiliations = _affiliations?.length ? `ownerAffiliations: [${_affiliations.map(x => x.toLocaleUpperCase()).join(", ")}]` : ""
 
     //Initialization
     const repositories = {list: []}
+    const processed = new Set()
 
-    //Fetch pinned repositories
-    if (pinned) {
-      const {user: {pinnedItems: {edges}}} = await graphql(queries.repositories.pinned({login, limit: pinned}))
-      repositories.list.push(...edges.map(({node}) => format(node)))
-    }
-
-    //Fetch repositories informations
+    //Featured repositories
     for (const repo of featured) {
       const {owner = login, name} = repo.match(/^(?:(?<owner>[\s\S]*)[/])?(?<name>[\s\S]+)$/)?.groups ?? {}
       const {repository} = await graphql(queries.repositories.repository({owner, name}))
-      repositories.list.push(format(repository))
+      repositories.list.push(format(repository, {sorting:"featured"}))
+      processed.add(repository.nameWithOwner)
     }
+
+    //Fetch pinned repositories
+    if (pinned) {
+      const {user: {pinnedItems: {edges}}} = await graphql(queries.repositories.pinned({login, limit: 6}))
+      repositories.list.push(...edges.map(({node}) => {
+        if (processed.has(node.nameWithOwner))
+          return null
+        processed.add(node.nameWithOwner)
+        return format(node, {sorting: "pinned"})
+      }).filter(repository => repository).slice(0, pinned))
+    }
+
+    //Fetch starred repositories
+    if (starred) {
+      const {user:{ repositories: {nodes}}} = await graphql(queries.repositories.starred({login, limit: Math.min(starred+10, 100), affiliations}))
+      let count = 0
+      for (const node of nodes) {
+        if (processed.has(node.nameWithOwner))
+          continue
+        const [owner, name] = node.nameWithOwner.split("/")
+        const {repository} = await graphql(queries.repositories.repository({owner, name}))
+        repositories.list.push(format(repository, {sorting:"starred"}))
+        processed.add(repository.nameWithOwner)
+        if (++count >= starred)
+          break
+      }
+    }
+
+    //Fetch random repositories
+    if (random) {
+      const {user:{ repositories: {nodes}}} = await graphql(queries.repositories.random({login, affiliations}))
+      let count = 0
+      for (const node of imports.shuffle(nodes)) {
+        if (processed.has(node.nameWithOwner))
+          continue
+        const [owner, name] = node.nameWithOwner.split("/")
+        const {repository} = await graphql(queries.repositories.repository({owner, name}))
+        repositories.list.push(format(repository, {sorting:"random"}))
+        processed.add(repository.nameWithOwner)
+        if (++count >= random)
+          break
+      }
+    }
+
+    //Sorting
+    repositories.list = repositories.list.sort((a, b) => order.indexOf(a.sorting) - order.indexOf(b.sorting))
 
     //Results
     return repositories
@@ -35,7 +78,7 @@ export default async function({login, q, imports, graphql, queries, data, accoun
 }
 
 /**Format repository data */
-function format(repository) {
+function format(repository, {sorting} = {}) {
   //Format date
   const time = (Date.now() - new Date(repository.createdAt).getTime()) / (24 * 60 * 60 * 1000)
   let created = new Date(repository.createdAt).toDateString().substring(4)
@@ -44,6 +87,10 @@ function format(repository) {
   else if (time < 30)
     created = `${Math.floor(time)} day${time >= 2 ? "s" : ""} ago`
   repository.created = created
+
+  //Sorting
+  if (sorting)
+    repository.sorting = sorting
 
   return repository
 }
---
+++ tests/mocks/api/github/graphql/repositories.starred.mjs
@@ -0,0 +1,13 @@
+/**Mocked data */
+export default function({faker, query, login = faker.internet.userName()}) {
+  console.debug("metrics/compute/mocks > mocking graphql api result > repositories/starred")
+  return ({
+    user: {
+      repositories: {
+        nodes: [
+          {nameWithOwner: "lowlighter/metrics"},
+        ]
+      }
+    },
+  })
+}
