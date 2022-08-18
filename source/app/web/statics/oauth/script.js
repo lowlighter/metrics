4b35fc5169ce31a3550a58f8bdd793c09a346ea9
lowlighter <22963968+lowlighter@users.noreply.github.com>
2022-08-17 20:23:17 -0400

    fix(app/web): add disclaimer about using localstorage

--- source/app/web/statics/oauth/script.js
+++ source/app/web/statics/oauth/script.js
@@ -74,6 +74,21 @@
       scopes: [],
       extras: [],
       session: null,
+      supported:(() => {
+        try {
+          const storage = window.localStorage
+          const test = "__storage_test__"
+          storage.setItem(test, test);
+          if (localStorage.getItem(test) !== test)
+            throw new Error("localStorage value mismatch")
+          storage.removeItem(test)
+          return true
+        }
+        catch (error) {
+          console.error(error)
+          return false
+        }
+      })()
     },
   })
 })()
