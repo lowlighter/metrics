2616c077fbda14a3f13c7efa3e248f8af7e49af7
cloydlau <253055734@qq.com>
2022-10-19 10:41:55 +0800

    feat(plugins/leetcode): add plugin_leetcode_url

--- source/plugins/leetcode/index.mjs
+++ source/plugins/leetcode/index.mjs
@@ -7,20 +7,20 @@ export default async function({login, q, imports, data, queries, account}, {enab
       return null
 
     //Load inputs
-    let {user, sections, "limit.skills": _limit_skills, "limit.recent": _limit_recent} = imports.metadata.plugins.leetcode.inputs({data, account, q})
+    let {user, sections, "limit.skills": _limit_skills, "limit.recent": _limit_recent, url} = imports.metadata.plugins.leetcode.inputs({data, account, q})
     const result = {user, sections, languages: [], skills: [], problems: {}, recent: []}
 
     //Languages stats
     {
       console.debug(`metrics/compute/${login}/plugins > leetcode > querying api (languages statistics)`)
-      const {data: {data: {matchedUser: {languageProblemCount: languages}}}} = await imports.axios.post("https://leetcode.com/graphql", {variables: {username: user}, query: queries.leetcode.languages()})
+      const {data: {data: {matchedUser: {languageProblemCount: languages}}}} = await imports.axios.post(`${url}/graphql`, {variables: {username: user}, query: queries.leetcode.languages()})
       result.languages = languages.map(({languageName: language, problemsSolved: solved}) => ({language, solved}))
     }
 
     //Skills stats
     {
       console.debug(`metrics/compute/${login}/plugins > leetcode > querying api (skills statistics)`)
-      const {data: {data: {matchedUser: {tagProblemCounts: skills}}}} = await imports.axios.post("https://leetcode.com/graphql", {variables: {username: user}, query: queries.leetcode.skills()})
+      const {data: {data: {matchedUser: {tagProblemCounts: skills}}}} = await imports.axios.post(`${url}/graphql`, {variables: {username: user}, query: queries.leetcode.skills()})
       for (const category in skills)
         result.skills.push(...skills[category].map(({tagName: name, problemsSolved: solved}) => ({name, solved, category})))
       result.skills.sort((a, b) => b.solved - a.solved)
@@ -30,7 +30,7 @@ export default async function({login, q, imports, data, queries, account}, {enab
     //Problems
     {
       console.debug(`metrics/compute/${login}/plugins > leetcode > querying api (problems statistics)`)
-      const {data: {data: {allQuestionsCount: all, matchedUser: {submitStatsGlobal: {acSubmissionNum: submissions}}}}} = await imports.axios.post("https://leetcode.com/graphql", {variables: {username: user}, query: queries.leetcode.problems()})
+      const {data: {data: {allQuestionsCount: all, matchedUser: {submitStatsGlobal: {acSubmissionNum: submissions}}}}} = await imports.axios.post(`${url}/graphql`, {variables: {username: user}, query: queries.leetcode.problems()})
       for (const {difficulty, count} of all)
         result.problems[difficulty] = {count, solved: 0}
       for (const {difficulty, count: solved} of submissions)
@@ -40,7 +40,7 @@ export default async function({login, q, imports, data, queries, account}, {enab
     //Recent submissions
     {
       console.debug(`metrics/compute/${login}/plugins > leetcode > querying api (recent submissions statistics)`)
-      const {data: {data: {recentAcSubmissionList: submissions}}} = await imports.axios.post("https://leetcode.com/graphql", {variables: {username: user, limit: _limit_recent}, query: queries.leetcode.recent()})
+      const {data: {data: {recentAcSubmissionList: submissions}}} = await imports.axios.post(`${url}/graphql`, {variables: {username: user, limit: _limit_recent}, query: queries.leetcode.recent()})
       result.recent = submissions.map(({title, timestamp}) => ({title, date: new Date(timestamp * 1000)}))
     }
 
