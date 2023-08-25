d2a73059b5e0b11ccbb273742a0445fcede48fa0
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-08-24 23:37:29 -0400

    fix(ci): markdown screenshot example

--- .github/scripts/markdown_example.mjs
+++ .github/scripts/markdown_example.mjs
@@ -13,8 +13,10 @@ const page = await browser.newPage()
 //Select markdown example and take screenshot
 await page.setViewport({width: 600, height: 600})
 await page.goto("https://github.com/lowlighter/metrics/blob/examples/metrics.markdown.md")
+await page.waitForSelector("article.markdown-body")
+await new Promise(solve => setTimeout(solve, 4000))
 const clip = await page.evaluate(() => {
-  const {x, y, width, height} = document.querySelector("#readme").getBoundingClientRect()
+  const {x, y, width, height} = document.querySelector("article.markdown-body").getBoundingClientRect()
   return {x, y, width, height}
 })
 await page.screenshot({type: "png", path: "/tmp/metrics.markdown.png", clip, omitBackground: true})
