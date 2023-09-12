54d67ecbab2ac1493266fdce24b7187d3506366c
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-09-12 17:41:50 -0400

    fix(plugins/lines): don't wait twice

--- source/plugins/lines/index.mjs
+++ source/plugins/lines/index.mjs
@@ -30,7 +30,7 @@ export default async function({login, data, imports, rest, q, account}, {enabled
       response = [...await Promise.allSettled(repositories.map(async ({repo, owner}) => imports.filters.repo(`${owner}/${repo}`, skipped) ? {handle: `${owner}/${repo}`, stats: (await rest.repos.getContributorsStats({owner, repo})).data} : {}))].filter(({status}) => status === "fulfilled").map((
         {value},
       ) => value)
-      if (delay) {
+      if ((delay)&&(!i)) {
         console.debug(`metrics/compute/${login}/plugins > lines > waiting ${delay}s while waiting for contributor stats to be updated`)
         await new Promise(resolve => setTimeout(resolve, delay * 1000))
       }
