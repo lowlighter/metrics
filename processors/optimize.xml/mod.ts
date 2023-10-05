d161d3e3bee1f9642b546f57d7f92e2e50d6072b
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-10-05 03:48:05 +0200

    feat(processors/optimize.xml): add support

---
+++ processors/optimize.xml/mod.ts
@@ -0,0 +1,27 @@
+// Imports
+import { Processor, state } from "@processor"
+import xmlFormat from "y/xml-formatter@3.5.0"
+
+/** Processor */
+export default class extends Processor {
+  /** Import meta */
+  static readonly meta = import.meta
+
+  /** Name */
+  readonly name = "🧹 Optimize XML"
+
+  /** Category */
+  readonly category = "optimizer"
+
+  /** Description */
+  readonly description = "Format XML to improve diff readability and size"
+
+  /** Supports */
+  readonly supports = ["application/xml", "image/svg+xml"]
+
+  /** Action */
+  protected async action(state: state) {
+    const result = await this.piped(state)
+    result.content = xmlFormat(result.content, { indentation: "  ", collapseContent: true, ignoredPaths: [] })
+  }
+}
