0f1b5e0808b85055539c0ca4e979eedb632981ed
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-10-05 08:10:42 +0200

    feat: add secret to censor token from logs

---
+++ source/metrics/utils/secret.ts
@@ -0,0 +1,16 @@
+/** Secret, used to prevent leaks in logs by hiding its value */
+export class Secret {
+  /** Constructor */
+  constructor(value: unknown) {
+    this.#value = `${value}`
+  }
+
+  /** Value */
+  readonly #value
+
+  /** Read value */
+  read() {
+    return this.#value
+  }
+
+}
