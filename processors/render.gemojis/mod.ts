9d6fed56c445c74a297f0b78baca58aa05332e85
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-10-05 03:49:13 +0200

    feat(processors/render.gemojis): add support

---
+++ processors/render.gemojis/mod.ts
@@ -0,0 +1,34 @@
+// Imports
+import { Processor, state } from "@processor"
+// @ts-ignore: json import
+import imported from "https://api.github.com/emojis" assert { type: "json" }
+const gemojis = imported as Record<string, string>
+
+/** Processor */
+export default class extends Processor {
+  /** Import meta */
+  static readonly meta = import.meta
+
+  /** Name */
+  readonly name = "🖌️ Render GitHub emojis"
+
+  /** Category */
+  readonly category = "renderer"
+
+  /** Description */
+  readonly description = "Render emojis supported by GitHub"
+
+  /** Supports */
+  readonly supports = ["application/xml", "image/svg+xml"]
+
+  /** Action */
+  protected async action(state: state) {
+    const result = await this.piped(state)
+    result.content = result.content.replaceAll(/:(?<emoji>[-\w+]+):/g, (match, emoji) => {
+      if (!(emoji in gemojis)) {
+        return match
+      }
+      return `<img class="gemoji" src="${gemojis[emoji]}" alt="${emoji}" />`
+    })
+  }
+}
