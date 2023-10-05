66898bd510f24ebfbd4c1850bc0a944d61da676d
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-10-05 03:46:54 +0200

    feat(plugins/gists): v4

---
+++ plugins/gists/mod.ts
@@ -0,0 +1,63 @@
+// Imports
+import { is, Plugin, state } from "@plugin"
+
+/** Plugin */
+export default class extends Plugin {
+  /** Import meta */
+  static readonly meta = import.meta
+
+  /** Name */
+  readonly name = "🎫 Gists"
+
+  /** Category */
+  readonly category = "github"
+
+  /** Supports */
+  readonly supports = ["user"]
+
+  /** Description */
+  readonly description = "Displays [GitHub gists](https://gist.github.com)"
+
+  /** Inputs */
+  readonly inputs = is.object({
+    forks: is.boolean().default(false).describe("Whether to include forks"),
+    visibility: is.enum(["public", "all"]).default("public").describe("Gists visibility"),
+  })
+
+  /** Outputs */
+  readonly outputs = is.object({
+    count: is.number().int().min(0).describe("Total number of gists"),
+    forked: is.number().int().min(0).describe("Total number of forked gists"),
+    comments: is.number().int().min(0).describe("Total number of comments"),
+    files: is.number().int().min(0).describe("Total number of files"),
+    forks: is.number().int().min(0).describe("Total number of forks"),
+    stargazers: is.number().int().min(0).describe("Total number of stargazers"),
+  })
+
+  /** Action */
+  protected async action({ errors }: state) {
+    const { handle } = this.context
+    const { forks, visibility } = await this.inputs.parseAsync(this.context.args)
+    const { entity: { gists: { count, nodes: gists } } } = await this.graphql("gists", { login: handle, privacy: visibility.toLocaleUpperCase() }, { paginate: true })
+    const result = { count, forked: 0, comments: 0, files: 0, forks: 0, stargazers: 0 }
+    let missing = 0
+    for (const gist of gists) {
+      if (!gist) {
+        missing++
+        continue
+      }
+      if ((gist.forked) && (!forks)) {
+        continue
+      }
+      result.forked += gist.forked ? 1 : 0
+      result.stargazers += gist.stargazers
+      result.forks += gist.forks.count
+      result.comments += gist.comments.count
+      result.files += gist.files.length
+    }
+    if (missing) {
+      errors.push({ severity: "warning", source: this.id, message: `${missing} gists could not be correctly fetched.\nProvided token may not have sufficient permissions.` })
+    }
+    return result
+  }
+}
---
+++ plugins/gists/tests/gists.graphql.ts
@@ -0,0 +1,32 @@
+import { faker, is, mock } from "@testing"
+
+export default mock({ login: is.string(), privacy: is.enum(["PUBLIC", "ALL"]) }, ({ privacy }) => {
+  const count = { PUBLIC: 10, ALL: 20 }[privacy]
+  return {
+    entity: {
+      gists: {
+        count,
+        nodes: [
+          ...Array.from({ length: count }, (_, i) => ({
+            stargazers: faker.number.int({ max: 100 }),
+            forked: i % 2 === 0,
+            forks: {
+              count: faker.number.int({ max: 100 }),
+            },
+            files: Array.from({ length: faker.number.int({ min: 1, max: 5 }) }, () => ({
+              name: faker.system.fileName(),
+            })),
+            comments: {
+              count: faker.number.int({ max: 100 }),
+            },
+          })),
+          null,
+        ],
+        pageInfo: {
+          hasNextPage: false,
+          endCursor: null,
+        },
+      },
+    },
+  }
+})
