b2df94232208f5dd2c9ff0600d892e72dae130c2
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-10-05 03:47:07 +0200

    feat(plugins/rss): v4

---
+++ plugins/rss/mod.ts
@@ -0,0 +1,51 @@
+// Imports
+import { is, Plugin } from "@plugin"
+import RSS from "y/rss-parser@3.13.0"
+
+/** Plugin */
+export default class extends Plugin {
+  /** Import meta */
+  static readonly meta = import.meta
+
+  /** Name */
+  readonly name = "🗼 Rss feed"
+
+  /** Category */
+  readonly category = "social"
+
+  /** Supports */
+  readonly supports = ["user", "organization", "repository"]
+
+  /** Description */
+  readonly description = "Displays entries from a RSS feed"
+
+  /** Inputs */
+  readonly inputs = is.object({
+    feed: is.string().url().describe("RSS feed (e.g. `https://news.ycombinator.com/rss`)"),
+    limit: is.number().int().min(1).nullable().default(4).describe("Display limit (use `null` to disable)"),
+  })
+
+  /** Outputs */
+  readonly outputs = is.object({
+    name: is.string().describe("Feed name"),
+    description: is.string().describe("Feed description"),
+    entries: is.array(is.object({
+      title: is.string().describe("Entry title"),
+      link: is.string().url().describe("Entry link"),
+      date: is.date().nullable().describe("Entry date"),
+    })).describe("Feed entries"),
+  })
+
+  /** Action */
+  protected async action() {
+    const { feed, limit } = await this.inputs.parseAsync(this.context.args)
+    const content = await this.fetch(feed, { type: "text" })
+    const { title: name, description, items } = await (new RSS()).parseString(content)
+    let entries = items.map(({ title, link, isoDate: date }) => ({ title, link, date: date ? new Date(date) : null }))
+    this.log.debug(`found ${entries.length} entries from ${feed}`)
+    if (limit) {
+      entries = entries.slice(0, limit)
+    }
+    return { name, description, entries }
+  }
+}
---
+++ plugins/rss/tests/rss.http.ts
@@ -0,0 +1,23 @@
+import { faker, mock } from "@testing"
+
+export default mock({}, () => {
+  return `
+    <rss version="2.0">
+      <channel>
+        <title>${faker.company.name()}</title>
+        <link>${faker.internet.url()}</link>
+        <description>${faker.company.catchPhrase()}</description>
+        <item>
+          <title>${faker.company.buzzPhrase()}</title>
+          <link>${faker.internet.url()}</link>
+          <pubDate>Sat, 16 Sep 2023 19:48:15 +0000</pubDate>
+        </item>
+        <item>
+          <title>${faker.company.buzzPhrase()}</title>
+          <link>${faker.internet.url()}</link>
+          <pubDate>Sat, 16 Sep 2023 22:27:08 +0000</pubDate>
+        </item>
+      </channel>
+    </rss>
+  `.trim()
+})
