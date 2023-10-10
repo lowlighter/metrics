234e51044f49a7ffbe04eb7382edc908ad537f95
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-10-10 00:26:43 -0400

    tests: no need to sanitize ops anymore

--- source/run/mod_test.ts
+++ source/run/mod_test.ts
@@ -1,5 +1,5 @@
 //Imports
-import { afterAll, assert, beforeAll, describe, it } from "@testing"
+import { assert, describe, it } from "@testing"
 import { Plugin } from "@plugin"
 import { Processor } from "@processor"
 import { process } from "@metrics/process.ts"
@@ -23,23 +23,10 @@ const config = {
   },
 }
 
-const shared = { process: null as null | Deno.ChildProcess, controller: new AbortController() }
-
-beforeAll(async () => {
-  const command = new Deno.Command("deno", { args: ["task", "browser", "--allow-write"], signal: shared.controller.signal })
-  Object.assign(shared, { process: command.spawn() })
-  Browser.endpoint = await read(".test/browser")
-})
-
-afterAll(async () => {
-  await Browser.close()
-  shared.controller.abort()
-  await shared.process?.status
-})
-
 //TODO(@lowlighter): test for engine
-//TODO(@lowlighter): check if puppeteer leaking test is fixable...
-const options = { sanitizeResources: false, sanitizeOps: false }
+
+// Clean browser after each test
+Browser.shared = false
 
 // Plugins
 for (const id of await Plugin.list()) {
@@ -51,7 +38,7 @@ for (const id of await Plugin.list()) {
     it.skip(name, () => void null)
     continue
   }
-  describe({ ...options, name }, () => {
+  describe({ name }, () => {
     for (const test of tests) {
       describe(test.name, () => {
         for (const template of templates) {
@@ -73,7 +60,7 @@ for (const id of await Processor.list()) {
     it.skip(name, () => void null)
     continue
   }
-  describe({ ...options, name }, () => {
+  describe({ name }, () => {
     for (const test of tests) {
       it(test.name, async () => {
         assert(await process(deepMerge(config, test, { arrays: "replace" })))
