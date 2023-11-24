8bb9d18f561584b6654101fbfbac814ccd97a9ab
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-11-24 04:19:22 -0500

    Delete test.ts

--- test.ts
+++
@@ -1,4 +0,0 @@
-const { Requests } = await import("@engine/components/requests.ts")
-const requests = new Requests(import.meta, { logs: "trace", token: { read: () => "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" } } as any)
-
-console.log(await requests.rest(requests.api.users.getByUsername, { username: "github" }))
