66edb7fa217e03e92f588882be32de6f833282fd
bvandercar-vt <bvandercar@outlook.com>
2025-10-16 19:28:46 -0600

    fix: undefined commit

--- source/plugins/languages/analyzer/recent.mjs
+++ source/plugins/languages/analyzer/recent.mjs
@@ -67,7 +67,7 @@ export class RecentAnalyzer extends Analyzer {
       ...await Promise.allSettled(
         commits
           .flatMap(({payload}) => payload.commits)
-          .filter(({committer}) => filters.text(committer?.email, this.authoring, {debug: false}))
+          .filter((commit) => commit?.committer?.email && filters.text(commit.committer.email, this.authoring, {debug: false}))
           .map(commit => commit.url)
           .map(async commit => (await this.rest.request(commit)).data),
       ),
